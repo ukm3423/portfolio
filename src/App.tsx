90b5edb2271a10d4d147786319770a969de07333
ukm3423 <ukm3423@gmail.com>
2025-06-20 13:35:25 +0530

    app.jsx updated

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,10 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
-import Navbar from './components/Navbar';
 
 
 const App: React.FC = () => (
   <Router>
-    {/* <Navbar/> */}
     <Routes>
       <Route path="/portfolio" element={<Home />} />
       
